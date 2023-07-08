af92b02b816c2728c070b9dea7dd9ef31cd71c3a
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-07-08 20:32:07 +0700

    Add event to rock button that successfully plays the game. Add result div that displays the result logic of the round. Refactor lines of code that previously was logged to the console which is now being swapped for DOM methods

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 function getComChoice() {
-    const strings = ['Rock', 'Paper', 'Scissors'];
+    const strings = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * strings.length);
     return strings[randomIndex]
 }
-
-// const userSelection = getUserInput();
-// const comSelection = getComChoice();
-;
-function getUserInput() {
-    let input = prompt('Type Rock, Paper or Scissors', '');
-return input;
+function getUserChoice() {
+    return rock.value;
 }
+
+
 
 function play(userSelection, comSelection) {
 
     
     userSelection = userSelection.toLowerCase();
-comSelection = comSelection.toLowerCase();
+// comSelection = comSelection.toLowerCase();
 
 if (userSelection === 'rock' && comSelection === 'scissors') {
     return `You Win! ${userSelection} beats ${comSelection}!`;
@@ -58,38 +55,50 @@ function checkLose(string) {
 
 function game() {
 
-    let a = play(getUserInput(), getComChoice());
-console.log(a);
+    let a = play(getUserChoice(), getComChoice());
+results.textContent = a;
 checkWin(a);
 checkLose(a);
 
 
-let b = play(getUserInput(), getComChoice());
-console.log(b);
-checkWin(b);
-checkLose(b);
+// let b = play(getUserInput(), getComChoice());
+// console.log(b);
+// checkWin(b);
+// checkLose(b);
 
-let c = play(getUserInput(), getComChoice());
-console.log(c);
-checkWin(c);
-checkLose(c);
+// let c = play(getUserInput(), getComChoice());
+// console.log(c);
+// checkWin(c);
+// checkLose(c);
 
-let d = play(getUserInput(), getComChoice());
-console.log(d);
-checkWin(d);
-checkLose(d);
+// let d = play(getUserInput(), getComChoice());
+// console.log(d);
+// checkWin(d);
+// checkLose(d);
 
-let e = play(getUserInput(), getComChoice());
-console.log(e);
-checkWin(e);
-checkLose(e);
+// let e = play(getUserInput(), getComChoice());
+// console.log(e);
+// checkWin(e);
+// checkLose(e);
 
  if (counterWin > counterLose) {
- return `Winner! You won ${counterWin} times & lost ${counterLose} times`;
+    return `Winner! You won ${counterWin} times & lost ${counterLose} times`;
  } else if (counterLose > counterWin) {
     return `Loser! You won ${counterLose} times & lost ${counterWin} times`;
  } else {
     return `It\'s a Draw! You won ${counterWin} times & lost ${counterLose} times`;
  }
- 
 }
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+
+
+rock.addEventListener('click', game)
+// paper.addEventListener('click', play('paper', getComChoice()))
+// scissors.addEventListener('click', play('scissors', getComChoice()))
+
+const results = document.querySelector('#results')
+results.textContent = counterWin; 
